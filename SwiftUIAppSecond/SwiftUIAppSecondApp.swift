a6fde903af42b9d672a6cd3b1356f9e8d1b8df47
Alexey Barinov <begoodbebad2012@gmail.com>
2021-06-03 20:35:29 +0300

    Initial Commit

---
+++ SwiftUIAppSecond/SwiftUIAppSecondApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppSecondApp.swift
+//  SwiftUIAppSecond
+//
+//  Created by Mac on 03.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppSecondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
